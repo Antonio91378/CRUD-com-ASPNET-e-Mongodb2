5b61646b0144eb4ab2056f62469e478f3c86f7aa
Antonio Dias <antoniovictor200@gmail.com>
2022-09-14 13:10:50 -0300

    att

---
+++ Blog.Infra/Contexts/Context2.cs
@@ -0,0 +1,14 @@
+﻿using API.Blog.BackEnd.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Blog.BackEnd.Infra.Contexts
+{
+    public class Context2 : DbContext
+    {
+        public Context2(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Comment>? comments { get; set; }
+    }
+}
